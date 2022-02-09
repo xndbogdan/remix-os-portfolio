3c7d5bfae9b132dfbfc30199af32342d916533c6
Bobee <bogdan@artgames.ro>
2022-02-10 00:18:49 +0200

    Player functionality done

--- app/components/MusicPlayer.js
+++ app/components/MusicPlayer.js
@@ -10,25 +10,82 @@ class MusicPlayer extends React.Component {
             selectedTrack: this.props.tracklist.payload[0].tracks_in_order[0],
             display: 'Player Offline',
             isPlaying: false,
+            trackProgress: 0,
+            currentTrackTime: 0,
+            currentTrackDuration: 0,
         };
         this.audio = React.createRef()
+        this.displayText = React.createRef()
+        this.displayTextContainer = React.createRef()
+        this.progressBar = React.createRef()
+        this.progressBarContainer = React.createRef()
         console.log(this.state)
     }
     render(props) {
         return (
         <div className="px-2">
-            <div className="bg-gray-900 h-8 border-2 border-gray-600 text-blue-300 px-2 flex items-center my-2">
-                <span className="opacity-75 whitespace-nowrap overflow-auto scrollbars-hidden">{this.state.display}</span>
+            <div className="bg-gray-900 h-8 border-2 border-gray-600 text-blue-300 px-2 flex items-center my-2" ref={this.displayTextContainer}>
+                <a target="_blank" href={this.state.selectedTrack.permalink_url} className="opacity-75 truncate cursor-point" ref={this.displayText}>{this.state.display}</a>
             </div>
             <p className="text-xs">Station: {this.state.selectedPlaylist.name}</p>
-            <div className="flex flex-row space-x-4 text-sm mt-2 pb-2">
-                <button>Previous</button>
-                <button onClick={this.togglePlay}>{this.state.isPlaying ? 'Pause' : 'Play'}</button>
-                <button>Next</button>
+            <div className="w-full h-2 bg-black cursor-point" ref={this.progressBarContainer} onMouseUp={this.updateSongPosition}>
+                <div ref={this.progressBar} className="bg-blue-300 h-2 pointer-events-none" style={{width:this.state.trackProgress}}></div>
             </div>
-            <audio ref={this.audio} src={'https://api.poolsidefm.workers.dev/v2/get_sc_mp3_stream?track_id=' + this.state.selectedTrack.soundcloud_id}></audio>
+            <div style={this.state.currentTrackDuration ? {display:'block'} : {display:'none'}}>{this.convertDuration(this.state.currentTrackTime)} / {this.convertDuration(this.state.currentTrackDuration)}</div>
+            <div className="flex flex-row space-x-4 text-sm mt-2 pb-2">
+                <button onClick={this.previousTrack}>
+                    <svg className="icon h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 9"> 
+                        <path fill="var(--color-icon, #000)" d="M12 0v9h-1V8h-1V7H9V6H8V5H7v4H6V8H5V7H4V6H3V5H2v4H0V0h2v4h1V3h1V2h1V1h1V0h1v4h1V3h1V2h1V1h1V0h1z"/>
+                    </svg>
+                </button>
+                <button onClick={this.togglePlay}>{!this.state.isPlaying ? 
+                    <svg className="icon h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 9 9">
+						<path fill="var(--color-icon, #000)" d="M3 9V0h1v1h1v1h1v1h1v1h1v1H7v1H6v1H5v1H4v1H3z"/>
+					</svg> 
+                    : 
+                    <svg className="icon h-4" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg" >
+						<path d="M2 0H4V9H2V0Z" fill="var(--color-icon, #000)" />
+						<path d="M5 0H7V9H5V0Z" fill="var(--color-icon, #000)" />
+					</svg>}
+                </button>
+                <button onClick={this.nextTrack}>
+                    <svg className="icon h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 9" >
+							<path fill="var(--color-icon, #000)" d="M0 9V0h1v1h1v1h1v1h1v1h1V0h1v1h1v1h1v1h1v1h1V0h2v9h-2V5H9v1H8v1H7v1H6v1H5V5H4v1H3v1H2v1H1v1H0z"/>
+					</svg>
+                </button>
+            </div>
+            <audio ref={this.audio} onEnded={this.nextTrack} onTimeUpdate={this.updateTrackProgress} src={'https://api.poolsidefm.workers.dev/v2/get_sc_mp3_stream?track_id=' + this.state.selectedTrack.soundcloud_id}></audio>
         </div>
         );
+    }
+
+    updateSongPosition = (event) => {
+        let boundingRect = event.target.getBoundingClientRect()
+        let percentage = ((event.clientX - boundingRect.left) / boundingRect.width)
+        this.audio.current.currentTime = percentage * this.audio.current.duration
+    }
+
+    convertDuration = (time) => {
+        let mins = Math.floor(time / 60);
+        if (mins < 10) {
+            mins = '0' + String(mins);
+        }
+        let secs = Math.floor(time % 60);
+        if (secs < 10) {
+            secs = '0' + String(secs);
+        }
+
+        return mins + ':' + secs;
+    }
+
+    updateTrackProgress = (event) => {
+        const currentTime = event.target.currentTime
+        const duration = event.target.duration
+        this.setState({
+            trackProgress: (currentTime + 0.25) / duration * 100 + '%',
+            currentTrackDuration: duration,
+            currentTrackTime: currentTime,
+        })
     }
 
     componentDidMount() {
@@ -44,11 +101,48 @@ class MusicPlayer extends React.Component {
     }
 
     togglePlay = () => {
-        this.setState({ play: !this.state.isPlaying }, () => {
-            console.log(this.state.isPlaying)
-            !this.state.isPlaying ? this.audio.current.play() : this.audio.current.pause();
-            this.state.isPlaying = !this.state.isPlaying
+        this.setState({ isPlaying: !this.state.isPlaying }, () => {
+            this.state.isPlaying ? this.audio.current.play() : this.audio.current.pause();
         });
+    }
+
+    nextTrack = () => {
+        if(this.state.trackIndex >= this.state.selectedPlaylist.tracks_in_order.length - 1) {
+            return;
+        }
+        let replay = false;
+        if(this.state.isPlaying) {
+            this.togglePlay()
+            replay = true
+        }
+        this.setState({
+            selectedTrack: this.state.selectedPlaylist.tracks_in_order[this.state.trackIndex+1],
+            trackIndex: this.state.trackIndex+1,
+        }, () => {
+            if(replay) {
+                this.togglePlay()
+            }
+        })
+        
+    }
+
+    previousTrack = () => {
+        if(this.state.trackIndex <= 0) {
+            return;
+        }
+        let replay = false;
+        if(this.state.isPlaying) {
+            togglePlay()
+            replay = true
+        }
+        this.setState({
+            selectedTrack: this.state.selectedPlaylist.tracks_in_order[this.state.trackIndex-1],
+            trackIndex: this.state.trackIndex-1
+        }, () => {
+            if(replay) {
+                this.togglePlay()
+            }
+        })
     }
 
     tick() {
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -3,9 +3,72 @@ import { useLoaderData } from "remix";
 import Clock from '~/components/Clock';
 import Calendar from '~/components/Calendar';
 import MusicPlayer from '~/components/MusicPlayer';
+import LRUCache from 'lru-cache';
 
 export let loader = async () => {
-  const data = await fetch('https://api.poolsidefm.workers.dev/v1/get_tracks_by_playlist');
+  const options = {
+    // the number of most recently used items to keep.
+    // note that we may store fewer items than this if maxSize is hit.
+    max: 500,
+  
+    // if you wish to track item size, you must provide a maxSize
+    // note that we still will only keep up to max *actual items*,
+    // so size tracking may cause fewer than max items to be stored.
+    // At the extreme, a single item of maxSize size will cause everything
+    // else in the cache to be dropped when it is added.  Use with caution!
+    // Note also that size tracking can negatively impact performance,
+    // though for most cases, only minimally.
+    maxSize: 5000,
+  
+    // function to calculate size of items.  useful if storing strings or
+    // buffers or other items where memory size depends on the object itself.
+    // also note that oversized items do NOT immediately get dropped from
+    // the cache, though they will cause faster turnover in the storage.
+    sizeCalculation: (value, key) => {
+      // return an positive integer which is the size of the item,
+      // if a positive integer is not returned, will use 0 as the size.
+      return 1
+    },
+  
+    // function to call when the item is removed from the cache
+    // Note that using this can negatively impact performance.
+    dispose: (value, key) => {
+      freeFromMemoryOrWhatever(value)
+    },
+  
+    // max time to live for items before they are considered stale
+    // note that stale items are NOT preemptively removed by default,
+    // and MAY live in the cache, contributing to its LRU max, long after
+    // they have expired.
+    // Also, as this cache is optimized for LRU/MRU operations, some of
+    // the staleness/TTL checks will reduce performance, as they will incur
+    // overhead by deleting items.
+    // Must be a positive integer in ms, defaults to 0, which means "no TTL"
+    ttl: 1000 * 60 * 5,
+  
+    // return stale items from cache.get() before disposing of them
+    // boolean, default false
+    allowStale: false,
+  
+    // update the age of items on cache.get(), renewing their TTL
+    // boolean, default false
+    updateAgeOnGet: false,
+  
+    // update the age of items on cache.has(), renewing their TTL
+    // boolean, default false
+    updateAgeOnHas: false,
+  
+    // update the "recently-used"-ness of items on cache.has()
+    // boolean, default false
+    updateRecencyOnHas: false,
+  }
+  const cache = new LRUCache(options)
+  let data = cache.get('tracklist')
+  if(!data) {
+    data = await fetch('https://api.poolsidefm.workers.dev/v1/get_tracks_by_playlist');
+    cache.set('tracklist', data)
+  }
+  
   // return userTimeline;
   return data;
 };
