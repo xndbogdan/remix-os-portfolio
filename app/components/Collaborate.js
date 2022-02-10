32725db0633c9ca879e02b700271167987544941
Bobee <bogdan@artgames.ro>
2022-02-10 22:51:32 +0200

    Fix layout

--- app/components/Collaborate.js
+++ app/components/Collaborate.js
@@ -14,16 +14,15 @@ class Collaborate extends React.Component {
     render() {
 			return (
 				<div className="flex flex-wrap items-center">
-					<div className="w-full md:w-1/6 justify-center items-center hidden md:flex">
-						<img className="h-16" src="/img/globe.gif"/>
+                    <p className="text-lg font-bold">Let's build something together!</p>
+					<div className="w-1/3 md:w-2/6 justify-center items-center">
+						<img className="h-16 w-auto mx-auto" src="/img/globe.gif"/>
 					</div>
-					<div className="w-full md:w-5/6">
-						<p className="text-lg font-bold">Let's build something together!</p>
-						<p className="mt-4 text-sm">I'm open to freelance jobs, collaboration with companies, startups and brands.</p>
-						<p className="font-bold text-sm">Current time in Bucharest: { this.state.time }</p>
-						<a className="cursor-point text-sm text-blue-600 mt-4" href="mailto:bogdan.mosteanu@hey.com" target="_blank">bogan.mosteanu@hey.com</a>
-						
+					<div className="w-2/3 md:w-4/6">
+						<p className="my-2 text-sm text-justify">I'm open to freelance jobs, collaboration with companies, startups and brands.</p>
 					</div>
+                    <p className="text-sm">Current time in Bucharest: { this.state.time }</p>
+                    <a className="cursor-point text-sm text-blue-600" href="mailto:bogdan.mosteanu@hey.com" target="_blank">bogan.mosteanu@hey.com</a>	
 				</div>
 			);
     }
