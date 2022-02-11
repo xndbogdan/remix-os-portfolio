a7423b9ebdeadb885a2716d6ee51ffea71b2de7c
Bobee <bogdan@artgames.ro>
2022-02-11 02:15:07 +0200

    Fixed typos

--- app/components/Desktop.js
+++ app/components/Desktop.js
@@ -177,8 +177,8 @@ class Desktop extends React.Component {
                                             <p className="mb-2 text-base">• About me</p>
                                             <p >I'm Bogdan, an independent full-stack developer from Bucharest.</p>
                                             <p>Most of my projects were made using Laravel, Vue and Tailwind.</p>
-                                            <p>I like making music, and goofy projects like this one.</p>
-                                            <p>I'm into Sci-Fi movies and video games, and dream of the 90's.</p>
+                                            <p>I like making music and goofy projects, like this one.</p>
+                                            <p>I'm into Sci-Fi movies, video games, and dream of the 90's.</p>
                                             <img className="my-2" src="/img/ship.gif"></img>
 
                                             <p className="my-2 text-base mt-2">• Work experience</p>
@@ -191,13 +191,12 @@ class Desktop extends React.Component {
                                             <p>Learned the basics of PHP and Symfony framework.</p>
 
                                             <p className="mt-2"><span className="text-blue-700">Full Stack Web Developer</span> at <a href="http://artgames.ro/" target="_blank">Art Games</a> (Jul 2018 - Oct 2019)</p>
-                                            <p>Auction house with proxy and live bidding.</p>
                                             <p>Worked on many internal projects, including <a target="_blank" href="https://artmark.ro">Artmark.ro</a>, and <a target="_blank" href="https://artsafari.ro">Artsafari.ro</a> and <a target="_blank" href="https://www.sothebysrealty.ro/">Sotheby's Realty Romania</a> .</p>
                                             <p>Started working on Artmark since it's beginning, to the end. Worked solo on many internal projects.</p>
                                             <p>Gained valuable experience with Laravel, Laravel Nova (also building Resource Tools), Laravel Scout, Vue.js, Vuex, Websockets, AWS services and Elasticsearch.</p>
 
                                             <p className="mt-2"><span className="text-blue-700">Full Stack Web Developer</span> at <a href="https://meevo.ca/" target="_blank">Meevo Digital</a> (Nov 2019 - Dec 2019)</p>
-                                            <p>Web development agency outsourcing for big clients Capgemini.</p>
+                                            <p>Web development agency outsourcing for big clients like Capgemini.</p>
                                             <p>Gained in-depth knowledge on wordpress development and sage theme builder.</p>
 
                                             <p className="mt-2"><span className="text-blue-700">Independent Contractor</span> (Aug 2019 - Present day)</p>
