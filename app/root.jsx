95758a4849ad4cb83958cd3fe8bd57ef6e0d2d00
Bobee <bogdan@artgames.ro>
2022-02-21 10:27:53 +0200

    Finished up some seo

--- app/root.jsx
+++ app/root.jsx
@@ -24,6 +24,17 @@ export default function App() {
         <title>Remix OS</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta property="og:title" content="Remix OS - My Portfolio" />
+        <meta property="og:type" content="profile" />
+        <meta property="og:url" content="https://remixos.dev/" />
+        <meta property="og:image" content="https://remixos.dev/remix.png" />
+        <meta property="og:site_name" content="Remix OS"></meta>
+        <meta name="twitter:title" content="Remix OS - My portfolio">
+        <meta name="twitter:description" content="My web development portfolio">
+        <meta name="twitter:image" content="https://remixos.dev/remix.png">
+        <meta name="twitter:image:alt" content="Remix framework logo"></meta>
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        
         <Meta />
         <Links />
       </head>
