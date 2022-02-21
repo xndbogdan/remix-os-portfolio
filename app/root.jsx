51ec1522da3d38019aa94dacb7b1dc5e6a50eeea
Bobee <bogdan@artgames.ro>
2022-02-21 10:33:03 +0200

    Fix meta tags

--- app/root.jsx
+++ app/root.jsx
@@ -28,12 +28,12 @@ export default function App() {
         <meta property="og:type" content="profile" />
         <meta property="og:url" content="https://remixos.dev/" />
         <meta property="og:image" content="https://remixos.dev/remix.png" />
-        <meta property="og:site_name" content="Remix OS"></meta>
-        <meta name="twitter:title" content="Remix OS - My portfolio">
-        <meta name="twitter:description" content="My web development portfolio">
-        <meta name="twitter:image" content="https://remixos.dev/remix.png">
-        <meta name="twitter:image:alt" content="Remix framework logo"></meta>
-        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta property="og:site_name" content="Remix OS"/>
+        <meta name="twitter:title" content="Remix OS - My portfolio"/>
+        <meta name="twitter:description" content="My web development portfolio"/>
+        <meta name="twitter:image" content="https://remixos.dev/remix.png"/>
+        <meta name="twitter:image:alt" content="Remix framework logo"/>
+        <meta name="twitter:card" content="summary_large_image"/>
         
         <Meta />
         <Links />
