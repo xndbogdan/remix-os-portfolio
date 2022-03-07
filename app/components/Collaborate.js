e5886fcf6fbbff3418ac44f38f26bc08abce54d6
Bobee <bogdan@artgames.ro>
2022-03-07 19:28:09 +0200

    Fix typo & responsiveness on collaborate section

--- app/components/Collaborate.js
+++ app/components/Collaborate.js
@@ -15,15 +15,18 @@ class Collaborate extends React.Component {
 	render() {
 		return (
 			<div className="flex flex-wrap items-center">
-				<p className="text-base md:text-lg">Let's build something together!</p>
+				<p className="text-base md:text-lg text-center w-full">Let's build something together!</p>
 				<div className="w-1/3 md:w-2/6 justify-center items-center">
 					<LazyLoadImage className="h-16 w-auto mx-auto" src="/img/globe.gif" alt="globe image"/>
 				</div>
-				<div className="w-2/3 md:w-4/6">
-					<p className="my-2 text-xs md:text-sm text-justify">I'm open to freelance jobs, collaboration with companies, startups and brands.</p>
+				<div className="w-2/3 md:w-4/6 my-2">
+					<p className="my-2 text-xs md:text-sm text-left md:text-justify">I'm open to freelance jobs, collaboration with companies, startups and brands.</p>
 				</div>
-				<p className="text-xs md:text-sm">Current time in Bucharest: { this.state.time }</p>
-				<a className="cursor-point text-xs md:text-sm text-blue-600" href="mailto:bogdan.mosteanu@hey.com" target="_blank">bogan.mosteanu@hey.com</a>	
+				<div className="flex flex-col text-center w-full">
+					<p className="text-xs md:text-sm">Current time in Bucharest: { this.state.time }</p>
+					<a className="cursor-point text-xs md:text-sm text-blue-600" href="mailto:bogdan.mosteanu@hey.com" target="_blank">bogan.mosteanu@hey.com</a>	
+				</div>
+
 			</div>
 		);
 	}
