5f0eb1eaf009ff225ab6883d0f5eeb94546e5f88
Bogdan Mihai <bogdan.mosteanu@hey.com>
2023-07-14 15:36:49 +0300

    refactor: improve resume

--- app/components/Resume.js
+++ app/components/Resume.js
@@ -5,42 +5,18 @@ import { jsPDF } from "jspdf";
 import html2canvas from 'html2canvas';
 
 class Resume extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {isDownloading: false};
-    }
-
     render() {
-        const isDownloading = this.state.isDownloading
-        let downloadButton = null
-        if (this.props.download && !isDownloading) {
-            downloadButton = (
-                <a className='flex flex-row items-center mt-2 cursor-point' onMouseDown={this.saveToFile}>
-                    <img src='/img/floppy.svg' alt='Save to file' className='w-6 h-6'/> <span className='text-blue-600 hover:text-blue-700 cursor-pointer ml-2 font-bold'>Save to file</span>
-                </a> 
-            )
-        }
-        if(isDownloading) {
-            downloadButton = (
-                <div className='flex flex-row items-center mt-2'>
-                    <img src='/img/floppy.svg' alt='Save to file' className='w-6 h-6'/> <span className='text-blue-600 ml-2 font-bold'>Downloading...</span>
-                </div> 
-            )
-        }
         return (
             <div>
-                {downloadButton}
                 <div id='resume-content'>
                     <h1 className='text-2xl mb-2 mt-4'>Contact Information</h1>
                     <p><span className='font-bold'>Personal Website: </span><a className='text-blue-600' target="_blank" href="https://remixos.dev">remixos.dev</a></p>
                     <p><span className='font-bold'>Phone Number: </span><a className='text-blue-600' target="_blank" href="tel:+40723400149">+40723400149</a></p>
                     <p><span className='font-bold'>E-Mail: </span><a className='text-blue-600' target="_blank" href="mailto:bogdan.mosteanu@hey.com">bogdan.mosteanu@hey.com</a></p>
+                    <p><span className='font-bold'>Name: </span>Bogdan-Mihai Moșteanu</p>
 
-                    <h1 className='text-2xl mb-2 mt-4'>Presentation</h1>
+                    <h1 className='text-2xl mb-2 mt-4'>Overview</h1>
                     <p>I'm Bogdan, a Full-Stack Developer & hobbyist composer from Bucharest.</p>
-                    <p className='mt-1'>Most of my projects were made using Laravel, Vue and Tailwind.</p>
-                    <p>I've also worked with Python (Cli apps & Flask apps) and Node (Express). I'm okay any tech stack, as long as it makes sense and it's supported.</p>
                     <p className='mt-1'>I want to build clean codebases that are easy to extend and work with.</p>
                     <p>I'm also a big fan of open-source software and I try to contribute to the community when I have the time.</p>
 
@@ -49,7 +25,7 @@ class Resume extends React.Component {
                     
                     <img id='ship' className="my-2" src="/img/ship.gif"/>
 
-                    <h1 className="text-2xl mb-2 mt-4">Work experience</h1>
+                    <h1 className="text-2xl mb-2 mt-4">Contract experience</h1>
                     
                     <p className="mt-2"><span className="text-blue-700">► Independent Contractor / Freelancer</span> (Nov 2019 - Present day)</p>
                     <p>Switched to contracting to enjoy the freedom of remote work and a larger variety of projects.</p>
@@ -66,6 +42,7 @@ class Resume extends React.Component {
                                 One of the most exciting aspects of this role has been my involvement in re-making part of the project with Vue3 & GraphQL and the opportunity to work with a bunch of technologies for the first time.<br/> 
                                 This has allowed me to expand my skillset and contribute to the ongoing success of the platform.
                             </p>
+                            <p className='mt-4'><span className='font-bold'>Tech Stack:</span> SST · AWS Lambda · MySQL · GraphQL · TypeScript · Amazon Web Services (AWS) · Node.js · JavaScript</p>
                         </div>
                         <div className='mt-4'>
                             <a className='text-blue-700 hover:text-blue-600' href='https://voice.ai/' target='_blank'>→ Voice.ai</a>
@@ -73,6 +50,7 @@ class Resume extends React.Component {
                                 Contributed to the Voice.AI application by developing various features on the Frontend/Backend related to user interaction & user features.<br/>
                                 Got some hands-on experience with vuetify & desktop app development.<br/> 
                             </p>
+                            <p className='mt-4'><span className='font-bold'>Tech Stack:</span> MySQL · Vuetify · Electron.js · Laravel · Vue.js · Laravel Nova</p>
                         </div>
                         <div className='mt-4'>
                             <a className='text-blue-700 hover:text-blue-600' href='https://www.quest-global.com/' target='_blank'>→ Quest Global</a>
@@ -84,6 +62,7 @@ class Resume extends React.Component {
                                 Through this experience, I was able to improve my communication, project management, and team collaboration skills, as well as gain a deeper understanding of software development principles and best practices.<br/>
                                 Gained valuable experience in the corporate environment.
                             </p>
+                            <p className='mt-4'><span className='font-bold'>Tech Stack:</span> MySQL · SCSS · React.js · Neo4j · Laravel</p>
                         </div>
                         <div className='mt-4'>
                             <a className='text-blue-700 hover:text-blue-600' href='https://www.webfusion.ro/' target='_blank'>→ WEBFUSION</a>
@@ -97,6 +76,7 @@ class Resume extends React.Component {
                                 <p>- Helped junior devs understand the tech stack and get better at their job</p>
                                 <p>- Worked on a few projects solo, from start to finish</p>
                             </p>
+                            <p className='mt-4'><span className='font-bold'>Tech Stack:</span> MySQL · Inertia.js · Elasticsearch · Laravel · Vue.js · PHP · Tailwind CSS</p>
                         </div>
                         <div className='mt-4'>
                             <a className='text-blue-700 hover:text-blue-600' href='https://boostit.com/' target='_blank'>→ BoostIT HUB</a>
@@ -105,6 +85,7 @@ class Resume extends React.Component {
                                 As instructed by my project manager, I was responsible for identifying and fixing bugs, as well as implementing new features to enhance the functionality of the platform. Working collaboratively with my team members.<br/>
                                 My experience at BoostIT Hub allowed me to further develop my skills in Laravel and Vue.js, as well as gain a deeper understanding of software development principles and best practices.
                             </p>
+                            <p className='mt-4'><span className='font-bold'>Tech Stack:</span> MySQL · Laravel · Vue.js</p>
                         </div>
                         <div className='mt-4'>
                             <a className='text-blue-700 hover:text-blue-600' href='http://dawn.ro/' target='_blank'>→ Dawn Consulting Group</a>
@@ -112,6 +93,7 @@ class Resume extends React.Component {
                                 Dawn Consulting is a technology consulting firm that specializes in providing custom software development solutions to businesses across various industries. As a software developer at Dawn Consulting, my primary responsibility was to develop and implement software solutions for the company's clients using a variety of programming languages and frameworks.<br/>
                                 Specifically, in my role at Dawn Consulting, I have worked on various projects for the company's clients, utilizing my expertise in Express.js, Python, Node.js, Laravel, and JavaScript. I have worked closely with the company's CEO to understand client requirements and develop new features for their applications.<br/>
                             </p>
+                            <p className='mt-4'><span className='font-bold'>Tech Stack:</span> MySQL · Express.js · Python (Programming Language) · Node.js · Laravel · JavaScript</p>
                         </div>
                         <div className='mt-4'>
                             <a className='text-blue-700 hover:text-blue-600' href='https://artgames.ro/' target='_blank'>→ ART GAMES</a>
@@ -129,10 +111,11 @@ class Resume extends React.Component {
                                 <p>- Part of the main development team for major projects like Artmark, Sotheby's Romania</p>
                                 <p>- Part of the main development team for a project with a banking partner (cannot give details due to nda)</p>
                             </p>
+                            <p className='mt-4'><span className='font-bold'>Tech Stack:</span> Webrtc · MySQL · Inertia.js · Elasticsearch · WebSocket · Laravel · Vue.js · PHP · WordPress · Tailwind CSS</p>
                         </div>
                     </div>
                     
-
+                    <h1 className="text-2xl mb-2 mt-4">Work experience</h1>
                     <p className="mt-2"><span className="text-blue-700">► Full Stack Web Developer</span> at <a href="http://artgames.ro/" target="_blank">Art Games</a> (Jul 2018 - Oct 2019)</p>
                     <p>Worked on many internal projects, including <a target="_blank" href="https://artmark.ro">Artmark.ro</a>, and <a target="_blank" href="https://artsafari.ro">Artsafari.ro</a> and <a target="_blank" href="https://www.sothebysrealty.ro/">Sotheby's Realty Romania</a> .</p>
                     <p>Started working on Artmark since it's beginning, to the end. Worked solo on many internal projects.</p>
@@ -147,15 +130,15 @@ class Resume extends React.Component {
                     <p>Learned to manage a fast-paced workflow, delivering features for important clients like L'Oreal and Rail Cargo Group</p>
 
                     <h1 className="text-2xl mb-2 mt-4">Projects</h1>
-                    <p className="mb-2 text-red-600">Note: The list of projects is longer but not all can be mentioned due to NDAs or due to the projects not being public yet. Most of my private projects are made using Laravel and Vue.js</p>
+                    <p className="mb-2 text-red-600">Note: The list of projects is longer but not all can be mentioned due to NDAs or due to the projects not being public yet.</p>
                     <p className="mt-2"><a className="text-blue-700 hover:text-blue-700" target="_blank" href="https://github.com/xndbogdan/statamic-bard-text-color">► Bard Text Color</a> - Statamic Framework Plugin</p>
                     <a target="_blank" href="https://packagist.org/packages/xndbogdan/statamic-bard-text-color" rel="nofollow">
-                        <LazyLoadImage className="my-1" src="/img/bard-text-color.svg" alt="Total Downloads" />
+                        <LazyLoadImage className="my-1" src="https://img.shields.io/packagist/dt/xndbogdan/statamic-bard-text-color.svg" alt="Total Downloads" />
                     </a>
-                    <p>This is a statamic bard plugin that lets you change text color! It allows you to use the default tailwind palette, but you can also add your own. At the moment it's still under development, so please report any bugs that you encounter. New feature suggestions are welcomed too.</p>
+                    <p>This is a statamic bard plugin that lets you change text color!<br/>It allows you to use the default tailwind palette, but you can also add your own.<br/>It is under active development. New feature suggestions are welcomed.</p>
                     
                     <p className="mt-2"><a className="text-blue-700 hover:text-blue-700" target="_blank" href="https://voice.ai/">► Voice AI</a> - App & Website</p>
-                    <p className="mt-2">Part of the main development team for the website & desktop app. This is an AI-powered voice changer that makes you sound like various celebrities.</p>
+                    <p className="mt-2">Part of the main development team for the website & desktop app.<br/>This is an AI-powered voice changer that makes you sound like various celebrities.</p>
 
                     <p className="mt-2 mb-1"><a className="text-blue-700 hover:text-blue-700" target="_blank" href="https://www.artmark.ro/">► A10 by Artmark</a> - Website</p>
                     <span className="text-xs rounded-md py-1 px-2">~70k visits per month</span>
@@ -179,52 +162,12 @@ class Resume extends React.Component {
 
                     <p className="mt-2"><a className="text-blue-700 hover:text-blue-700" target="_blank" href="https://github.com/xndbogdan/laravel-ray-legacy">► Laravel Ray Legacy</a> - Laravel Plugin</p>
                     <a href="https://packagist.org/packages/xndbogdan/laravel-ray-legacy" rel="nofollow">
-                        <LazyLoadImage className="my-1" src="/img/ray.svg" alt="Total Downloads" />
+                        <LazyLoadImage className="my-1" src="https://img.shields.io/packagist/dt/xndbogdan/laravel-ray-legacy.svg" alt="Total Downloads" />
                     </a>
                     <p>This package can be installed in any PHP application to send messages to the Ray app. It was modified to work with older laravel installations.<br />It was tested on 5.6 but may work with even lower versions.</p>
                 </div>
             </div>
         );
-    }
-    saveToFile = () => {
-        this.setState({isDownloading: true});
-        let platform = 0 // 0 = pc, 1 = mac
-        if(navigator.platform.toLowerCase().indexOf("mac") != -1) platform = 1
-        console.log(platform)
-        
-        const doc = new jsPDF('p', 'mm', [300, 600]);
-        const resumeContent = document.getElementById('resume-content')
-        const shipImage = document.getElementById('ship')
-        resumeContent.style.width = '700px'
-        resumeContent.style.height = '8000px'
-        // decrease base font size of div to fit on one page if on pc
-        if(platform != 1) {
-            resumeContent.style.fontSize = '.6rem'
-        }
-        shipImage.style.display = 'none'
-
-        html2canvas(resumeContent, {
-            removeContainer: false,
-            useCORS: false,
-            allowTaint: false,
-            height: 8000,
-        }).then(canvas => {
-                
-    
-                var imgData = canvas.toDataURL('image/png');
-                doc.addImage(imgData, 'PNG', 15, 0);
-                doc.save('resume.pdf');
-                setTimeout(() => {
-                    this.setState({isDownloading: false});
-                    resumeContent.style.width = null
-                    resumeContent.style.height = null
-                    shipImage.style.display = null
-                    if(platform != 1) {
-                        resumeContent.style.fontSize = null
-                    }
-                }, 10)
-                
-        })
     }
 }
 export default Resume;
--- app/entry.client.jsx
+++ app/entry.client.jsx
@@ -1,4 +1,3 @@
-
 import { hydrate } from "react-dom";
 import { RemixBrowser } from "remix";
 
